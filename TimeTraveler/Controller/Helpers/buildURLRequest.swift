a607e34df8c1ac556859cfc3f60236eae6436bb3
Heemo <heemo521@gmail.com>
2022-12-24 11:58:29 -0800

    Create Url builder

--- TimeTraveler/Controller/Helpers/buildURLRequest.swift
+++ TimeTraveler/Controller/Helpers/buildURLRequest.swift
@@ -10,8 +10,17 @@ import Foundation
 struct buildURLRequest {
 
     static func getRequest(query: [String], parameters: [String: String]) {
-        print(query.first!)
         
+        //Build URL
+        let urlComponents = NSURLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "api.foursquare.com"
+        urlComponents.path = "/v3/places/search"
+        urlComponents.queryItems = parameters.map({ URLQueryItem(name: $0.key, value: $0.value)})
+        
+        //Final URL
+        guard let finalUrl = urlComponents.url else { return }
+        print(finalUrl)
     }
     
 }
