9bc27055e06c662aa1586c1dd94921f8e9569f3d
Heemo <heemo521@gmail.com>
2022-12-25 21:31:03 -0800

    Resizable image circle

--- TimeTraveler/Controller/HomeVC.swift
+++ TimeTraveler/Controller/HomeVC.swift
@@ -13,29 +13,55 @@ class HomeVC: UIViewController {
     @IBOutlet weak var mainImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var guideView: UIView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        scalingAnimation()
+//        imageViewContainer.translatesAutoresizingMaskIntoConstraints = false
         // Do any additional setup after loading the view.
+    }
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        scalingAnimation()
     }
 }
 
 private extension HomeVC {
+//    func viewOrientationChange() {
+//
+//    }
+    func scalingAnimation() {
+        if UIDevice.current.orientation.isLandscape {
+            print("lanscape")
+            UIView.animate(withDuration: 0.6, animations: {
+                self.imageViewContainer.transform = CGAffineTransform.identity
+            }, completion: { _ in
+                UIView.animate(withDuration: 0.6, animations: {
+                    self.imageViewContainer.transform = CGAffineTransform(scaleX: 0.6, y: 0.6)
+                })
+            })
+        } else {
+//          imageViewContainer.widthAnchor.constraint(equalToConstant: 150).isActive = false
+            print("portrait")
+            UIView.animate(withDuration: 0.6, animations: {
+                self.imageViewContainer.transform = CGAffineTransform(scaleX: 0.6, y: 0.6)
+            }, completion: { _ in
+                UIView.animate(withDuration: 0.6, animations: {
+                     self.imageViewContainer.transform = CGAffineTransform.identity
+                })
+            })
+        }
+    }
+    
     func updateUI() {
         // MARK: - Main Image Container
         
-        imageViewContainer.translatesAutoresizingMaskIntoConstraints = false
-        imageViewContainer.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-//        imageViewContainer.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        imageViewContainer.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 100).isActive = true
-        imageViewContainer.widthAnchor.constraint(equalToConstant: 300).isActive = true
-        imageViewContainer.heightAnchor.constraint(equalToConstant: 300).isActive = true
-        imageViewContainer.layer.cornerRadius =
-        imageViewContainer.frame.size.width / 2
+        imageViewContainer.layer.cornerRadius = imageViewContainer.frame.width / 2
         imageViewContainer.clipsToBounds = true
-        imageViewContainer.layer.borderColor = UIColor.white.cgColor
-        imageViewContainer.layer.borderWidth = 3
+        imageViewContainer.layer.borderColor = UIColor.gray.cgColor
+        imageViewContainer.layer.borderWidth = 1
         
     }
     
